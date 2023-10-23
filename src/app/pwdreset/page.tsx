7d115481d49b22fa74a3329a007403cf2af034ce
SantivqzV <a01177850@tec.mx>
2023-10-23 10:57:48 -0600

    Modified pwd reset page

--- src/app/pwdreset/page.tsx
+++ src/app/pwdreset/page.tsx
@@ -7,17 +7,20 @@ import React, {useEffect, useState} from "react";
 export default function PwdResetPage(){
     const [token, setToken] = useState("");
     const [password, setPassword] = useState("");
-    const [reset, setReset] = useState(false);
+    const [loading, setloading] = useState(false);
     const [error, setError] = useState(false);
 
     const resetPwd = async () => {
         try {
             await axios.post('/api/users/pwdreset', {token, password})
-            setReset(true);    
+            setloading(true);    
 
         } catch (error:any) {
             setError(true);
             console.log(error.response.data);
+        }
+        finally{
+            setloading(false);
         }
     }
 
@@ -27,9 +30,10 @@ export default function PwdResetPage(){
     }, [])
 
     return(
-        <div>
-            <h1>ResetPwd</h1>
-            <input type="password" placeholder="New Password" onChange={(e) => setPassword(e.target.value)}/>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>{loading ? "Processing" : "Reset Password"}</h1>
+            <hr /> 
+            <input className = "p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-blue-500" type="password" placeholder="New Password" onChange={(e) => setPassword(e.target.value)}/>
             <button onClick={resetPwd}>ResetPwd</button>
             <Link href="/login">Login</Link>
         </div>
